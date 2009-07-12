3179d8e044eff85a484cdb9ce5aafa160ffe6c4c
gray <gray@cpan.org>
2009-07-11 20:38:03 -0700

    use canonical rt links

--- lib/Geo/Coder/Bing.pm
+++ lib/Geo/Coder/Bing.pm
@@ -191,7 +191,7 @@ L<Geo::Coder::Google>, L<Geo::Coder::Yahoo>
 =head1 REQUESTS AND BUGS
 
 Please report any bugs or feature requests to
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Geo-Coder-Bing>. I will be
+L<http://rt.cpan.org/Public/Bug/Report.html?Queue=Geo-Coder-Bing>. I will be
 notified, and then you'll automatically be notified of progress on your bug
 as I make changes.
 
@@ -219,7 +219,7 @@ L<http://cpanratings.perl.org/d/Geo-Coder-Bing>
 
 =item * RT: CPAN's request tracker
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Geo-Coder-Bing>
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Geo-Coder-Bing>
 
 =item * Search CPAN
 
