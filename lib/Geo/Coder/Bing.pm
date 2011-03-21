f13974d635333ee692252f553721b87860147400
gray <gray@cpan.org>
2011-03-21 09:43:31 -0700

    less code

--- lib/Geo/Coder/Bing.pm
+++ lib/Geo/Coder/Bing.pm
@@ -21,12 +21,9 @@ sub new {
 
     my $self = bless \ %params, $class;
 
-    if ($params{ua}) {
-        $self->ua($params{ua});
-    }
-    else {
-        $self->{ua} = LWP::UserAgent->new(agent => "$class/$VERSION");
-    }
+    $self->ua(
+        $params{ua} || LWP::UserAgent->new(agent => "$class/$VERSION")
+    );
 
     if ($self->{debug}) {
         my $dump_sub = sub { $_[0]->dump(maxlength => 0); return };
