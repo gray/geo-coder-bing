920d1a48e76d5495e8e7e94356c5d046f10563b9
gray <gray@cpan.org>
2011-03-29 16:54:57 -0700

    require newer LWP with better https support

--- lib/Geo/Coder/Bing.pm
+++ lib/Geo/Coder/Bing.pm
@@ -34,9 +34,8 @@ sub new {
         $self->ua->default_header(accept_encoding => 'gzip,deflate');
     }
 
-    if ($self->{https} and not $self->ua->is_protocol_supported('https')) {
-        croak q('https' requires Crypt::SSLeay or IO::Socket::SSL)
-    }
+    croak q('https' requires LWP::Protocol::https)
+        if $self->{https} and not $self->ua->is_protocol_supported('https');
 
     return $self;
 }
@@ -166,7 +165,8 @@ geocoding service.
     $geocoder = Geo::Coder::Bing->new('Your Bing Maps key')
     $geocoder = Geo::Coder::Bing->new(
         key   => 'Your Bing Maps key',
-        https => 1,
+        # https => 1,
+        # debug => 1,
     )
 
 Creates a new geocoding object.
